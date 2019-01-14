899f346bad755d3262771ed5d8f86702db6857cb
Pratik Mazumdar <45979935+LUCIF680@users.noreply.github.com>
2019-01-14 10:53:23 +0530

    Update directdownload.h

--- directdownload.h
+++ directdownload.h
@@ -1,32 +1,35 @@
 #pragma once
 
-class Direct {
-	
-	public: static double convertBytesToKB(double bytes){
+class DownloadManager {
+
+	public: static double convertBytesToKB(double bytes) {
 		return bytes / 1024;
 	}
-	
+
 	public: static double convertKBToMB(long double KB) {
-		return (double) KB / 1024;
+		return (double)KB / 1024;
 	}
 
 	public: static long double convertMBToGB(long double MB) {
 		return MB / 1024;
 	}
-	
-	// use this in thread to get the current downloaded percentage.
-	public: static int percentDownload(const char* filename ,double real_file_size ){
-		std::ifstream in(filename, std::ios::binary | std::ios::ate);
-		double current_file_size = (double) in.tellg();
-		std::cout <<(current_file_size / real_file_size) * 100;
+
+	public: static void percentDownload(const char* filename, long real_file_size) {
+		long current_file_size;
+		do{
+			std::ifstream in(filename, std::ios::binary | std::ios::ate);
+			current_file_size = (long)in.tellg();
+			std::cout <<"\r" <<(current_file_size / real_file_size) * 100<<"%";
+		} while (real_file_size != current_file_size);
+		std::cout << "\n";
 	}
 
-	 // a replaceall function 
+			// a replaceall function 
 	private: static std::string replaceAll(std::string str, const std::string& from, const std::string& to) {
 		size_t start_pos = 0;
 		while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
 			str.replace(start_pos, from.length(), to);
-			start_pos += to.length(); 
+			start_pos += to.length();
 		}
 		return str;
 	}
@@ -36,8 +39,8 @@ class Direct {
 		std::size_t pos_one = link.find("/");
 		link = link.substr(0, pos_one);
 		reverse(link.begin(), link.end());
-		link = Direct::replaceAll(link,"%20"," ");
-		link = Direct::replaceAll(link, "%40", " ");
+		link = DownloadManager::replaceAll(link, "%20", " ");
+		link = DownloadManager::replaceAll(link, "%40", " ");
 		return link;
-		}
+	}
 };
